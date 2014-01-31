14114d96f6cd7032254f87968abd9a7f4421f28f
Phillip Friedrich <friedrich.phillip@gmail.com>
2014-01-31 16:47:36 +0100

    second

--- src/main/java/com/phillip/news/controller/HomeController.java
+++ src/main/java/com/phillip/news/controller/HomeController.java
@@ -2,8 +2,11 @@ package com.phillip.news.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+
+import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +15,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.phillip.news.domain.Media;
+import com.phillip.news.repository.MediaRepository;
 import com.phillip.news.utils.ImageUtils;
 
 /**
@@ -22,9 +27,10 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+
+	@Inject
+	private MediaRepository mediaRepo;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
@@ -38,6 +44,11 @@ public class HomeController {
 		
 		/*Map<String, String> links = ImageUtils.buildImageTree("http://images02.kurier.at/schumacher-000_APA_VALDRIN+XHEMAJ.jpg/314.454");
 		System.out.println(links);*/
+		
+		List<Media> media = mediaRepo.findAll();
+		for(Media medium : media){
+			System.out.println("id: " + medium.getId() + " / " + medium.getTitle() + " / " + medium.getImageMedium());
+		}
 		
 		return "home";
 	}
